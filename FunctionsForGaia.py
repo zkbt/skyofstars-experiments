8ba51007bdaceb448233d2de08d56a35a509f509
Zach Berta-Thompson <zach.bertathompson@colorado.edu>
2017-03-01 13:06:41 -0700

    bug fix - convert from millarcsecond to arcseconds for GAIA parallax

--- FunctionsForGaia.py
+++ FunctionsForGaia.py
@@ -5,43 +5,43 @@ from math import pi
 
 def toXYZ(RA,DEC,Distance): #,Parallax
     """
-    This function converts RA, Dec and Distance into 
+    This function converts RA, Dec and Distance into
     X,Y and Z coordinates.
-    
+
     Parameters:
     --------------------------------------------------
     RA = Array of the right ascencion of each star.
     DEC = Array of the declination of each star.
     Distance = Array of the distance from Earth of
     each star, in AU.
-    
+
     Returns:
     ---------------------------------------------------
     X,Y,Z arrays of the x,y,z coordinates of all the
     stars included, in parsecs.
-    
+
     """
     r=Distance*(np.cos(DEC))
     z=Distance*(np.sin(DEC))
     y=r*(np.sin(RA))
     x=r*(np.cos(RA))
-    
+
     return x,y,z
 
 def MagnitudeToFlux(M):
     """
     This function uses the visual magnitude of a group of star
     to find out their energy flux (power/area).
-    
-    Parameters: 
+
+    Parameters:
     -------------------
     M = numpy array of the magnitudes of a group of stars
-    
-    Returns: 
+
+    Returns:
     -------------------
-    Fluxes = numpy array of the fluxes each of the stars emits in units Flx 
+    Fluxes = numpy array of the fluxes each of the stars emits in units Flx
     (Need flux and magnitude of another star to convert in SI units Watts/m^2 )
-     
+
     """
     # Use magnitudes formula, need to use the flux of a Star with magnitude 0
     # this will make the formula return fluxes on that unit
@@ -55,40 +55,38 @@ def MagnitudeToFlux(M):
 
 def ParallaxToDistance(P):
     """
-    This function converts the parallax of a group 
-    of stars into their distance from Earth. 
-    
+    This function converts the parallax of a group
+    of stars into their distance from Earth.
+
     Parameters:
     ----------------------------------------------
     P = Array of the parallaxes of a group of stars in arcseconds
-    
+
     Returns:
     ----------------------------------------------
     Array of the distances from the Earth of a group of stars in AU
     """
     #Calculate distance in parseconds:
     distance = 1/P
-    
+
     #convert to AU:
     distanceAU= (distance)*(360/(2*pi)*3600)
-    
+
     return distanceAU
 
 def AbsoluteMagnitude(distances,magnitudes):
     """
-    Use aparent magnitudes and distances to calculate the 
+    Use aparent magnitudes and distances to calculate the
     absolute magnitude of the stars.
-    
-    Parameters: 
+
+    Parameters:
     ----------------------------------------
     Distances =  Array of the star's distances, calculated from the parallax
     of the Tgas files.
-    Magnitudes =  
-    
+    Magnitudes =
+
     """
     inParsec= distances*(1.0/(360.0/(2.0*pi)*3600.0))
     M = magnitudes-5*np.log10(inParsec/10)
     
     return M
-
-
--- FunctionsToLoadGaia.py
+++ FunctionsToLoadGaia.py
@@ -8,26 +8,26 @@ import astropy.io.ascii as a
 import astropy.io.fits
 import astropy.table
 
-#use module created specially for this: 
+#use module created specially for this:
 import FunctionsForGaia as F
 
 def extractStars(filename):
-    
+
     """
-    This function will read in a TGAS FITS file and extract the information that the 
+    This function will read in a TGAS FITS file and extract the information that the
     Sky Of Stars project will be working with.
     It will only use the data from stars whose parallax error is less than 20%.
-        
+
     Parameters:
     ------------------------------------------------------------------
     Filename = TGAS.fits file
-        
-    Returns: 
+
+    Returns:
     ------------------------------------------------------------------
     Arrays for X,Y and Z positions and arrays for right ascencion, declination, magnitudes,
-    distances and absolute magnitudes. 
+    distances and absolute magnitudes.
     label: A
-  
+
     """
     #Read the file into a table:
     hdu = astropy.io.fits.open(filename)
@@ -39,69 +39,70 @@ def extractStars(filename):
     ok2 = table['parallax']>0
     ok = ok1*ok2
     ##ok3???
-    
+
     #From the table, extract parallax and other useful info, take only those rows for which [ok] is true:
     Parallax= table['parallax'].data[ok] #.data takes only the numbers, getting rid of the tittle of the column
+    Parallax = Parallax/1000 # ZKBT: the GAIA parallaxes are in units of milliarcseconds; let's convert to arcsec
     Dec = (table['dec'].data[ok])*(pi/180) #changing RA and DEC to radians.
     RA = (table['ra'].data[ok])*(pi/180)
     Fluxes= table['phot_g_mean_flux'].data[ok]
     Magnitudes = table['phot_g_mean_mag'].data[ok]
-    
+
     #Use formulas to produce more useful arrays:
     Distances = F.ParallaxToDistance(Parallax)
     X,Y,Z= F.toXYZ(RA,Dec,Distances)
     AbsoluteMagnitudes = F.AbsoluteMagnitude(Distances,Magnitudes)
-    
+
     #Create a label dependant on the file name:
     temp = filename.replace('.fits','.png')
     tempList = temp.split('_')
     label = tempList[-1]
-    
+
     return X,Y,Z,RA,Dec,Magnitudes,Distances,AbsoluteMagnitudes,Fluxes,label
 
 def PlotStars(X,Y,Z,RA,Dec,Distances,Fluxes,label):
-    """ 
+    """
     This function creates demonstraation plots and saves each in a file with name
     dependent on the Gaia file it comes from
-    
-    Parameters: 
+
+    Parameters:
     -------------------------------------------------------------------
     X,Y,Z,RA,DEC,Distances arrays calculated or extracted using the function "extractStars".
     label: string dependant on file name, also obtained with the "extractStars"function.
-    
+
     Returns and Saves:
     --------------------------------------------------------------------
     4 plots: x vs y position, x vs z position, y vs z position and RA vs Dec.
-    The title of each plot 
-    
-    """    
+    The title of each plot
+
+    """
     plt.figure(figsize=(9,9))
     plt.scatter(X,Y,s = Fluxes/1000000,alpha=0.1)
     plt.xlim(np.min(X),np.max(X))
     plt.ylim(np.min(Y),np.max(Y))
     plt.xlabel('X Coordinates')
-    plt.ylabel('y coordinates')  
+    plt.ylabel('y coordinates')
     plt.title('X vs Y for {}'.format(label))
     plt.savefig('XY_{}'.format(label))
-    
+
     plt.figure(figsize=(9,9))
     plt.scatter(X,Z,s = Fluxes/1000000,alpha=0.1)
     plt.xlim(np.min(X),np.max(X))
     plt.ylim(np.min(Z),np.max(Z))
     plt.xlabel('X Coordinates')
-    plt.ylabel('Z coordinates')  
+    plt.ylabel('Z coordinates')
     plt.title('X vs Z for {}'.format(label)) #Make the graph title and filename depend on label.
     plt.savefig('XZ_{}'.format(label))
-    
+
     plt.figure(figsize=(9,9))
     plt.scatter(Y,Z,s = Fluxes/1000000,alpha=0.1)
     plt.xlim(np.min(Y),np.max(Y))
     plt.ylim(np.min(Z),np.max(Z))
     plt.xlabel('Y Coordinates')
-    plt.ylabel('Z coordinates')  
+    plt.ylabel('Z coordinates')
     plt.title('Y vs Z for {}'.format(label))
     plt.savefig('YZ_{}'.format(label))
-    
+
     plt.figure(figsize=(9,9))
     plt.subplot(111, projection="aitoff")
     plt.scatter(RA,Dec,color='aqua')
@@ -111,43 +112,43 @@ def PlotStars(X,Y,Z,RA,Dec,Distances,Fluxes,label):
     plt.title('RA and Dec for {}'.format(label))
     plt.savefig('RADec_{}'.format(label))
 
-def CreateTxtFile(X,Y,Z,AbsoluteMagnitudes,label): 
+def CreateTxtFile(X,Y,Z,AbsoluteMagnitudes,label):
     """
-    This functions create a text file that is label dependant, and contains a list of 
+    This functions create a text file that is label dependant, and contains a list of
     X,Y,and Z positions, and absolute magnitude of each star
-    
+
     Parameters:
     --------------------------------------------------------
     X,Y,Z,AbsoluteMagnitudes arrays.
     label:string dependant on file name, also obtained with the "extractStars"function.
-    
+
     Saves:
     ---------------------------------------------------------
     "label".txt file with X,Y,Z and absolute magnitudes list for each star.
     """
-    
+
     t = astropy.table.Table([X, Y,Z,AbsoluteMagnitudes], names=['X', 'Y','Z','Absolute Magnitudes'])
     a.write(t, '{}.txt'.format(label),format='fixed_width')
 
-def convertTgasFile(filename): 
-    
+def convertTgasFile(filename):
+
     """
     This function uses the functions extractStars, CreateTcxtFile and PlotStars
-    
+
     """
-    X,Y,Z,RA,Dec,Magnitudes,Distances,AbsoluteMagnitudes,Fluxes, label = extractStars(filename) 
+    X,Y,Z,RA,Dec,Magnitudes,Distances,AbsoluteMagnitudes,Fluxes, label = extractStars(filename)
     CreateTxtFile(X,Y,Z,AbsoluteMagnitudes,label)
     PlotStars(X,Y,Z,RA,Dec,Distances,Fluxes,label)
-    
+
     return "Good job Luci"
 
 def fileForFiske(filename):
     """
     This function uses creates a text file, whose name depends on the filename.
     The text file contains a column for each ID number, X,Y and Z positions, dx, dy, dz velocities, BV and absolute magnitude.
-    dx, dy, dz and bv are empty columns but serve as place holders. 
+    dx, dy, dz and bv are empty columns but serve as place holders.
     """
-    X,Y,Z,RA,Dec,Magnitudes,Distances,AbsoluteMagnitudes,Fluxes, label = extractStars(filename)
+    X,Y,Z,RA,Dec,Magnitudes,Distances,AbsoluteMagnitudes,Fluxes,label = extractStars(filename)
     ID = np.arange(len(X))
     DX = np.zeros_like(X)
     DY = np.zeros_like(X)
@@ -156,7 +157,6 @@ def fileForFiske(filename):
 
     table = astropy.table.Table([ID, X, Y, Z, DX, DY, DZ, BV, AbsoluteMagnitudes],
         names=['ID', 'X', 'Y', 'Z', 'DX', 'DY', 'DZ', 'BV', 'AbsMag'])
-                          
+
     # write out the table
-    astropy.io.ascii.write(table, 'GaiaFile{}forFiske.dat'.format(label),format='fixed_width',delimiter='\t')
- 
+    astropy.io.ascii.write(table, 'GaiaFile{}forFiske.dat'.format(label.split('.')[0]),format='fixed_width',delimiter='\t')
